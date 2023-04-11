78b6bb1bf1f1f0d7a86d250ada6be1a2e362937e
Vidoosh <bangvidoosh@gmail.com>
2023-04-11 10:22:36 +0530

    some more updates

---
+++ temp_app.py
@@ -0,0 +1,35 @@
+# import the necessary packages
+import time
+import board
+import adafruit_dht
+import psutil
+
+def sense():
+    # We first check if a libgpiod process is running. If yes, we kill it!
+    for proc in psutil.process_iter():
+        if proc.name() == 'libgpiod_pulsein' or proc.name() == 'libgpiod_pulsei':
+            proc.kill()
+
+    # Initial the dht device, with data pin connected to digital pin 23:
+    sensor = adafruit_dht.DHT11(board.D23)
+
+    flag = True
+    while flag:
+        try:
+            # Print the values to the serial port
+            temp = sensor.temperature
+            humidity = sensor.humidity
+            print("Temperature: {}*C   Humidity: {}% ".format(temp, humidity))
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        except RuntimeError as error:
+            print(error.args[0])
+            time.sleep(2.0)
+            continue
+        # Exit the program cleanly
+        except Exception as error:
+            sensor.exit()
+            raise error
+        # Wait a second before continuing
+        time.sleep(2.0)
+        flag = False
+    return temp, humidity
